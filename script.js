71b5763d8deed14df423260e3dcba1ee927c43a3
codder-sw <shivamwaghule2005@gmail.com>
2026-05-26 14:28:05 +0530

    Added SDD todo app files

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Task cannot be empty");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    let delBtn = document.createElement("button");
+    delBtn.textContent = "Delete";
+    delBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(delBtn);
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
